e0580f962379098b410a1347c7aa634a60e47770
Minea Chhuon <54653983+MineaChhuon@users.noreply.github.com>
2019-08-29 22:54:35 +0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,7 @@
+
+    // Get the current year for the copyright
+    $('#year').text(new Date().getFullYear());
+    
+    $('.port-item').click(function(){
+      $('.collapse').collapse('hide');
+    });
